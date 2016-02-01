1bac8ab8fd5026dbf138d40077fe522368cea2c6
krizoo <dev@whitehatboy.com>
2016-02-01 15:41:54 +0200

    added hello world to the driver project

---
+++ Driver.java
@@ -0,0 +1,13 @@
+
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
